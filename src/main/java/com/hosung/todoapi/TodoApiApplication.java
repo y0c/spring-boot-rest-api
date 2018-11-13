920ebd999478004996dc0bfead2cd99996ce677f
Hosung Lim <holnet1026@gmail.com>
2018-11-13 22:45:20 +0900

    modelmapper add

--- src/main/java/com/hosung/todoapi/TodoApiApplication.java
+++ src/main/java/com/hosung/todoapi/TodoApiApplication.java
@@ -1,10 +1,17 @@
 package com.hosung.todoapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoApiApplication {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TodoApiApplication.class, args);
